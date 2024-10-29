d3f3f610ea3f3171c76be4559eed596d924ae06f
Tanveer Alam <103511510+tanve0er@users.noreply.github.com>
2024-10-29 17:17:59 +0530

    Update app.js short

--- app.js
+++ app.js
@@ -3,16 +3,7 @@ const body = document.querySelector("body");
 
 buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
-   
-    const id = e.target.id;
-    if (id === "grey") {
-      body.style.backgroundColor = id;
-    } else if (id == "purple") {
-      body.style.backgroundColor = id;
-    } else if (id == "blue") {
-      body.style.backgroundColor = id;
-    } else if (id == "yellow") {
-      body.style.backgroundColor = id;
-    }
+    const color = e.target.id; 
+        body.style.backgroundColor = color;
   });
 });
